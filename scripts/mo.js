93c66ff5a41057ca82e2d474584ad8305d3a11c8
alexmansfield <alex@sackclothstudios.com>
2014-02-04 16:11:38 -0800

    Added support for switching on screen size, instead of element size

--- scripts/mo.js
+++ scripts/mo.js
@@ -3,7 +3,8 @@
 
 	var Menu = {
 		options: {
-			mobileBreakpoint: 600,
+			mobileBreakpoint: 900,
+			breakpointType: 'screen', // 'screen' or 'element'
 			mobileMenuLocation: '',
 			toggleButtonID: 'menu-toggle-button',
 			arrowClass: 'menu-arrows',
@@ -141,8 +142,13 @@
 		 * menu should change state.
 		 */
 		toggleMobile: function() {
-			var mo = this.options,
-				width = this.container.outerWidth();
+			var mo = this.options;
+
+			if ( 'screen' == mo.breakpointType ) {
+				var width = viewportSize.getWidth();
+			} else {
+				var width = this.container.outerWidth();
+			}
 
 			// Check if viewport width is less than the mobile breakpoint setting and the mobile menu is not displayed yet.
 			if ( width < mo.mobileBreakpoint && !this.el.hasClass(mo.mobileClass) ) {
@@ -209,3 +215,63 @@ jQuery(function($) {
 		toggleButtonID: 'menu-toggle-button2'
 	});
 });
+
+
+
+
+
+
+/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
+(function (window) {
+
+	window.viewportSize = {};
+
+	window.viewportSize.getHeight = function () {
+		return getSize("Height");
+	};
+
+	window.viewportSize.getWidth = function () {
+		return getSize("Width");
+	};
+
+	var getSize = function (Name) {
+		var size;
+		var name = Name.toLowerCase();
+		var document = window.document;
+		var documentElement = document.documentElement;
+		if (window["inner" + Name] === undefined) {
+			// IE6 & IE7 don't have window.innerWidth or innerHeight
+			size = documentElement["client" + Name];
+		} else if (window["inner" + Name] != documentElement["client" + Name]) {
+			// WebKit doesn't include scrollbars while calculating viewport size so we have to get fancy
+
+			// Insert markup to test if a media query will match document.doumentElement["client" + Name]
+			var bodyElement = document.createElement("body");
+			bodyElement.id = "vpw-test-b";
+			bodyElement.style.cssText = "overflow:scroll";
+			var divElement = document.createElement("div");
+			divElement.id = "vpw-test-d";
+			divElement.style.cssText = "position:absolute;top:-1000px";
+			// Getting specific on the CSS selector so it won't get overridden easily
+			divElement.innerHTML = "<style>@media(" + name + ":" + documentElement["client" + Name] + "px){body#vpw-test-b div#vpw-test-d{" + name + ":7px!important}}</style>";
+			bodyElement.appendChild(divElement);
+			documentElement.insertBefore(bodyElement, document.head);
+
+			if (divElement["offset" + Name] == 7) {
+				// Media query matches document.documentElement["client" + Name]
+				size = documentElement["client" + Name];
+			} else {
+				// Media query didn't match, use window["inner" + Name]
+				size = window["inner" + Name];
+			}
+		
+			// Cleanup
+			documentElement.removeChild(bodyElement);
+		} else {
+			// Default to use window["inner" + Name]
+			size = window["inner" + Name];
+		}
+		return size;
+	};
+
+})(this);
